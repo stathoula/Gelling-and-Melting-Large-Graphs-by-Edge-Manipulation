1e35cdbb6e1bb808fe6dc2b34955c236ff1a4d7d
Efstathia (Stathoula) Chioteli <t8150148@dias.aueb.gr>
2017-09-05 23:35:44 +0300

    Gelling-and-Melting-Large-Graphs

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Gelling-and-Melting-Large-Graphs-by-Edge-Manipulation
+# Gelling, and Melting, Large Graphs by Edge Manipulation
+
+This is an algorithm which is about **K-EdgeDeletion και K-EdgeAddition**. Is written in **Python 3.X** and is based on the article 
+Hanghang Tong, B. Aditya Prakash, Tina Eliassi-Rad, Michalis Faloutsos, and Christos Faloutsos. 2012. Gelling, and melting, large graphs by edge manipulation. In Proceedings of the 21st ACM international conference on Information and knowledge management (CIKM '12). ACM, New York, NY, USA, 245-254.
+
+You can find the **article** [here](http://www.cs.cmu.edu/~christos/PUBLICATIONS/cikm12-netgel.pdf).
+
+This work was given as a bonus project from [Panos Louridas](https://github.com/louridas) in course **Algorithms and Data Structures** (*Department of Forest Science and Technology*, Athens University of Economics and Business).
+You can find the assignment [here](http://nbviewer.jupyter.org/github/dmst-algorithms-course/assignment-2017-bonus/blob/master/assignment_2017_bonus.ipynb).
+
+To run the program:
+
+python gelling_melting.py [-h] [-g] [-d] [-s SEPARATOR] [-f] graph num_edges
+
+* positional arguments:
+  * graph		graph file
+  * num_edges	number of edges to delete / add
+
+* optional arguments:
+  * -h, --help			show this help message and exit
+  * -g, --gell			gell graph
+  * -d, --directed		directed graph
+  * -s, --separator		SEPARATOR
+  * -f, --figure		draw the graph before and after the k-edge deletion / addition
+
+**Example:** For the file [graph.txt](graph.txt) 
+`python gelling_melting.py graph.txt 2 -g -s -f:` the output is:
+
+```
+((1, 7), 0.14202618512757398)
+((1, 4), 0.14202618512757384)
+
+which mean that we added the edge 1-7 and 1-4
+
+```
+
+<img src="before.JPG" width="350"><img src="after.JPG" width="350">
+
+* The algorithm also works for big graphs. Specifically, it should be able to produce results for the newest [AS Graph Data Set](http://topology.eecs.umich.edu/archive/asgraph_new.tar.gz) (not directed, see [here](http://topology.eecs.umich.edu/data.html) for information), as well as for a [subset of the Facebook graph](https://snap.stanford.edu/data/facebook_combined.txt.gz) (not directed, see [here](https://snap.stanford.edu/data/egonets-Facebook.html) for information), or the [Twitter graph](https://snap.stanford.edu/data/twitter_combined.txt.gz) (directed, information [here](https://snap.stanford.edu/data/egonets-Twitter.html)). For example, on an Ocean Machine with 2 CPUs and 4 GB of RAM, less than 6 seconds are required for the Facebook graph and less than 1.5 minutes for the Twitter graph to identify 10 links to delete.
---
+++ gelling_melting.py
@@ -0,0 +1,109 @@
+import argparse
+import numpy as np
+import scipy as sc
+import networkx as nx
+import matplotlib.pyplot as plt
+import itertools
+from scipy import sparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gell", action="store_true" , help="gel graph")
+parser.add_argument("-d", "--directed", action="store_true" , help="directed graph")
+parser.add_argument("-s", "--separator" , help="seperator")
+parser.add_argument("-f", "--figure" ,  action="store_true", help="Figure")
+parser.add_argument("filename", help="name of input file")
+parser.add_argument("num_edges", help="number of edges to delete", type=int)
+args = parser.parse_args()
+num_edges = args.num_edges
+
+if not args.directed:
+	if args.separator:
+		s = args.separator
+		G=nx.read_edgelist(args.filename, delimiter = s, nodetype = int)
+	else:
+		G=nx.read_edgelist(args.filename, nodetype = int)
+else:
+	if args.separator:
+		s = args.separator
+		G=nx.read_edgelist(args.filename, delimiter = s, nodetype = int, create_using=nx.DiGraph())
+	else:
+		G=nx.read_edgelist(args.filename, nodetype = int, create_using=nx.DiGraph())
+
+nodes = [node for node in G.nodes()]
+# We find the coordinate of each node
+nodeToCoordinate = { node: count for count, node in enumerate(nodes) }
+
+adj_matrix = nx.to_scipy_sparse_matrix(G, dtype=np.float64, format='csr')
+#we calculate the max eigenvalue, left eigenvector right eigenvector
+max_eigenvalue, right_eigenvector= sc.sparse.linalg.eigs(adj_matrix, k=1, M=None, sigma=None, which='LR', v0=None, ncv=None, maxiter=None, tol=0, return_eigenvectors=True, Minv=None, OPinv=None, OPpart=None)
+#we calculate the transpose adjacency_matrix so as to find the left eigenvector
+adj_matrix_T = sc.sparse.csr_matrix.transpose(adj_matrix)
+max_eigenvalue, left_eigenvector= sc.sparse.linalg.eigs(adj_matrix_T, k=1, M=None, sigma=None, which='LR', v0=None, ncv=None, maxiter=None, tol=0, return_eigenvectors=True, Minv=None, OPinv=None, OPpart=None)
+# We now check for negative values of the max left eigenvector
+
+if np.min(left_eigenvector) < 0:
+	left_eigenvector = - left_eigenvector
+if np.min(right_eigenvector) < 0:
+	right_eigenvector = - right_eigenvector
+
+if not args.gell: #(K-edge Deletion)
+	#we  calculate the score of every edge
+	scores = {}
+	for edge in G.edges():
+		scores[(edge[0],edge[1])] = float(left_eigenvector[nodeToCoordinate[edge[0]]].real * right_eigenvector[nodeToCoordinate[edge[1]]].real)
+
+	#PRINT SOLUTION###
+	deletions = {} #A dictionary with the edges that we will add and their score in order to use it for args.figure
+	for x  in sorted(scores.items(), key=lambda x: x[1], reverse = True)[:num_edges]:
+		deletions[x[0]] = x[1]
+		print(str(x))
+
+else: #(K-edge Addition)
+	if (args.directed):
+		in_degrees = G.in_degree(G.nodes())
+		max_inDegree = max(in_degrees.values())
+		out_degrees = G.out_degree(G.nodes())
+		max_outDegree = max(out_degrees.values())
+	else:
+		degrees = G.degree(G.nodes())
+		max_degree = max(degrees.values())
+
+	# We find the subset of k + din nodes with the highest left eigenscores ui
+	# and the subset of k + dout nodes with the highest right eigenscores vi
+	if args.directed:
+		nodesI = np.argsort(-left_eigenvector.T, kind='quicksort')[:num_edges + max_inDegree].flatten()
+		nodesJ = np.argsort(-right_eigenvector.T, kind='quicksort')[:num_edges + max_outDegree].flatten()
+	else:
+		nodesI = np.argsort(-left_eigenvector.T, kind='quicksort')[:num_edges + max_degree].flatten()
+		nodesJ = np.argsort(-right_eigenvector.T, kind='quicksort')[:num_edges + max_degree].flatten()
+
+	# We find the coordinate of each node
+	coordinateToNode = { count: node for count, node in enumerate(nodes) }
+
+	#We calculate the score of the edges i,j E(nodesI, nodesJ)
+	scores = {}
+	combinations = itertools.product(nodesI,nodesJ)
+	for x in combinations:
+		if x[0] != x[1] and adj_matrix[(x[0], x[1])] == 0 and (coordinateToNode[x[1]],coordinateToNode[x[0]]) not in scores.keys():
+			scores[(coordinateToNode[x[0]],coordinateToNode[x[1]])] = float(left_eigenvector[x[0]].real * right_eigenvector[x[1]].real)
+
+	#PRINT SOLUTION###
+	additions = {} #A dictionary with the edges that we will add and their score in order to use it for args.figure
+	for x  in sorted(scores.items(), key=lambda x: x[1], reverse = True)[:num_edges]:
+		additions[x[0]] = x[1]
+		print(str(x))
+		
+#Print a small graph before and after the k-edge addition/k-edge addition if --figured
+if (args.figure):
+	fig = plt.figure()
+	fig.suptitle('BEFORE', fontsize=20)
+	nx.draw_networkx(G)
+	plt.show()
+	if (args.gell):
+		G.add_edges_from(additions.keys())
+	else:
+		G.remove_edges_from(deletions.keys())
+	fig = plt.figure()
+	fig.suptitle('AFTER', fontsize=20)
+	nx.draw_networkx(G)
+	plt.show()
